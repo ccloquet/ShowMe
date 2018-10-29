60a317f8a039b4bcd9c2b36ea2e08b30a6f7a08d
De Coninck Laurent <laurent@adlogix.eu>
2018-10-29 14:48:17 +0100

    add base code for error code

--- php/upload.php
+++ php/upload.php
@@ -1,64 +1,69 @@
 <?php
-	require('../shared/params.php');
-	require('../shared/manage_keys.php');
+    require('../shared/params.php');
+    require('../shared/manage_keys.php');
 
-	if (!isset($_GET['key'])) 					returns_error();
-	if (!verify_key($_GET['key']))					returns_error();
+    if (!isset($_GET['key'])) {
+        returns_error();
+    }
+    if (!verify_key($_GET['key'])) {
+        returns_error();
+    }
+    if (!isset($_FILES['upload_file'])) {
+        returns_error();
+    }
 
-	if (!isset($_FILES['upload_file'])) 				returns_error();
+    $key = $_GET['key'];
+    $userid = get_user_from_key($key);
 
-	$key 	= $_GET['key'];
-	$userid = get_user_from_key($key);
- 
-	if ($_FILES["upload_file"]["size"] > 50000000)		returns_error();
+    if ($_FILES["upload_file"]["size"] > 50000000) {
+        returns_error();
+    }
 
-	$tmp_name = $_FILES['upload_file']['tmp_name'];
+    $tmp_name = $_FILES['upload_file']['tmp_name'];
 
-	if (exif_imagetype($tmp_name) != IMAGETYPE_JPEG) 	returns_error();
+    if (exif_imagetype($tmp_name) != IMAGETYPE_JPEG) {
+        returns_error();
+    }
 
-	if (extension_loaded('imagick'))
-	{
-		$img  = new Imagick();
-    		$img->readImageFile($tmp_name);
-		/* -- from here does not work
-		$data = $img->identifyImage();
-		echo json_encode($data);
+    if (extension_loaded('imagick')) {
+        $img = new Imagick();
+        $img->readImageFile($tmp_name);
+        /* -- from here does not work
+        $data = $img->identifyImage();
+        echo json_encode($data);
 
-		if (!$data || strpos(strtolower($data['format']), 'jpeg') === FALSE) 
-		{
-			returns_error();
-		}*/
-	}
+        if (!$data || strpos(strtolower($data['format']), 'jpeg') === FALSE)
+        {
+            returns_error();
+        }*/
+    }
 
-	$type 	  = getimagesize($tmp_name);
+    $type = getimagesize($tmp_name);
 
-	if ( ($type === false) ||  (!in_array( $type[2], [ IMAGETYPE_JPEG ] ) ) )
-	{
-		returns_error();
-	}
+    if (($type === false) || (!in_array($type[2], [IMAGETYPE_JPEG]))) {
+        returns_error();
+    }
 
-	if (!file_exists(BASE_FOLDER . '/' . $userid))
-	{
-		mkdir(BASE_FOLDER . '/' . $userid, 0777, true);
-	}
+    if (!file_exists(BASE_FOLDER . '/' . $userid)) {
+        mkdir(BASE_FOLDER . '/' . $userid, 0777, true);
+    }
 
-	if(move_uploaded_file($tmp_name, BASE_FOLDER . '/' . $userid . '/' . $key . uniqid() . uniqid() . '.jpg'))
-	{
-		returns_ok();
-	} 
-	else 
-	{
-		returns_error();
-	}
+    if (move_uploaded_file($tmp_name, BASE_FOLDER . '/' . $userid . '/' . $key . uniqid() . uniqid() . '.jpg')) {
+        returns_ok();
+    } else {
+        returns_error();
+    }
 
-	function returns_error()
-	{
-		die('ERROR');
-	}
+    /**
+     * @param int $httpErrorCode
+     */
+    function returns_error($httpErrorCode = 400)
+    {
+        die('ERROR');
+    }
 
-	function returns_ok()
-	{
-		die('OK');
-	}
-
+    function returns_ok()
+    {
+        die('OK');
+    }
 ?>
