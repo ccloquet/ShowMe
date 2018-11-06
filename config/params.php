219780d7ed941db5cbb8b871ab8d70597c076cf9
De Coninck Laurent <laurent@adlogix.eu>
2018-11-06 17:39:55 +0100

    config file per environment

--- config/params.php
+++ config/params.php
@@ -1,12 +1,11 @@
 <?php
-	
-	define('BASE_URL', 		'https://****/index.html');				// URL OF THE CLIENT PAGE
-	define('BASE_FOLDER', 		'../received/');					// FOLDER WHERE THE IMAGES ARE STORED
+
+	define('BASE_URL', 		'http://192.168.98.100/html/index.html');				// URL OF THE CLIENT PAGE
+	define('BASE_FOLDER', 	'../received/');					// FOLDER WHERE THE IMAGES ARE STORED
+	define('ENV',           'dev');
 
 	$params = [
 		// userids (confidential)							// secret -- allow to authentify that the link sent by SMS comes from this server
-		'' =>	['secret'=> '', 'name'=> '', 'sms_apikey' => '']
 	];
 
-?>
-
+	require_once ('params.'.ENV.'.php');
