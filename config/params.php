086069c384a557d6b1b319d1eb5595d304c2fb04
Christophe Cloquet <ccloquet@users.noreply.github.com>
2018-11-16 23:56:26 +0100

    Update params.php

--- config/params.php
+++ config/params.php
@@ -6,6 +6,7 @@
 
 	$params = [
 		// userids (confidential)							// secret -- allow to authentify that the link sent by SMS comes from this server
+		// '' =>	['secret'=> '', 'name'=> '', 'sms_apikey' => '']
 	];
 
 	require_once ('params.'.ENV.'.php');
