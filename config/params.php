ea42c24fc2a938e315607a8d94d12fdc33ee1a0f
Christophe Cloquet <ccloquet@users.noreply.github.com>
2018-11-16 23:55:38 +0100

    Update params.php

--- config/params.php
+++ config/params.php
@@ -1,6 +1,6 @@
 <?php
 
-	define('BASE_URL', 		'http://192.168.98.100/html/index.html');				// URL OF THE CLIENT PAGE
+	define('BASE_URL', 		'https://****/index.html');				// URL OF THE CLIENT PAGE
 	define('BASE_FOLDER', 	'../received/');					// FOLDER WHERE THE IMAGES ARE STORED
 	define('ENV',           'dev');
 
