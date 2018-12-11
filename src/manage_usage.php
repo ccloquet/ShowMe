7da873ff8eadee0309918221906bd61201d8b6c5
ccloquet <c.cloquet@gmail.com>
2018-12-11 16:34:13 +0100

    USAGE_PATH->USAGE_FOLDER for consistency

--- src/manage_usage.php
+++ src/manage_usage.php
@@ -2,14 +2,14 @@
 	
 	function create_usage_file_if_needed($userid)					// not great, should be a database (linked to the credential database)
 	{
-		if (!is_dir(USAGE_PATH))						// what does it assumes about the parent dir? +x?
+		if (!is_dir(USAGE_FOLDER))						// what does it assumes about the parent dir? +x?
 		{
-			mkdir(USAGE_PATH, 0777, true);
+			mkdir(USAGE_FOLDER, 0777, true);
 		}
 
-		if (sizeof(glob(USAGE_PATH . $userid . '_*.usage')) === 0) 		// file structure: $userid . '_' . random string . '.usage' => can be stored in a potentially web-exposed folder but no one can see it
+		if (sizeof(glob(USAGE_FOLDER . $userid . '_*.usage')) === 0) 		// file structure: $userid . '_' . random string . '.usage' => can be stored in a potentially web-exposed folder but no one can see it
 		{
-			$filename = USAGE_PATH . $userid  . '_' . bin2hex(random_bytes(64)) . '.usage';
+			$filename = USAGE_FOLDER . $userid  . '_' . bin2hex(random_bytes(64)) . '.usage';
 
 			file_put_contents($filename, time() . ','.INITIAL_TOPUP.PHP_EOL);		
 		}
@@ -19,7 +19,7 @@
 	{
 		create_usage_file_if_needed($userid);
 
-		$filenames   = glob(USAGE_PATH . $userid . '_*.usage');
+		$filenames   = glob(USAGE_FOLDER . $userid . '_*.usage');
 		file_put_contents($filenames[0], time().',' . $credits.PHP_EOL, FILE_APPEND);
 	}
 
@@ -27,7 +27,7 @@
 	{
 		create_usage_file_if_needed($userid);
 
-		$filenames   = glob(USAGE_PATH . $userid . '_*.usage');		
+		$filenames   = glob(USAGE_FOLDER . $userid . '_*.usage');		
 
 		$usage_array = array_map('str_getcsv', file($filenames[0]));
 		
@@ -45,7 +45,7 @@
 	{
 		create_usage_file_if_needed($userid);
 
-		$filenames   = glob(USAGE_PATH . $userid . '_*.usage');
+		$filenames   = glob(USAGE_FOLDER . $userid . '_*.usage');
 		file_put_contents($filenames[0], time().',' . $credits.PHP_EOL, FILE_APPEND);
 	}
 
