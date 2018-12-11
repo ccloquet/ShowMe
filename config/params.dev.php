6a19404ff987d72030eea449e936e9d8f279e22a
ccloquet <c.cloquet@gmail.com>
2018-12-11 16:35:48 +0100

    adaptation of the config to the mods (merge of the folders,usage_mgt)

--- config/params.dev.php
+++ config/params.dev.php
@@ -1,11 +1,19 @@
 <?php
 
-    define('TWILIO_SID', '');
-    define('TWILIO_APIKEY', '');
+	define('TWILIO_SID', '');
+	define('TWILIO_APIKEY', '');
 
-    define('BASE_URL', 		'https://****/index.html');				// URL OF THE CLIENT PAGE
-	define('BASE_FOLDER', 	'../received/');					// FOLDER WHERE THE IMAGES ARE STORED
+	define('BASE_URL', 		'https://****/received/showme.html');		// URL OF THE CLIENT PAGE
+
+	define('BASE_FOLDER', 		'../received/');				// FOLDER WHERE THE IMAGES ARE STORED
+	define('USAGE_FOLDER', 		'../db/');					// FOLDER WHERE THE USAGE DATA ARE STORED
+
+	define ("INITIAL_TOPUP", 10);							// by default, gives 5 credits the first time
+
 
     $params = array_merge($params, [
         1 =>   ['secret'=> 'azerty', 'name'=> 'test user', 'sms_apikey' => '']
     ]);
+
+    // a way to generate keys: 
+    //for i in {1..20}; do head -n 8192 /dev/urandom | openssl sha256; sleep 1; done
--- received/params.js
+++ received/params.js
@@ -1,2 +1,2 @@
-var base_url    = 'https://***/html/index.html';
+var client_path = 'https://***/php/'	// PATH the scripts
 var peerjs_url  = '***.herokuapp.com';	// PEER JS SERVER
