697bb5c3c6038aff2a9d78ac58f35f14f73b31f4
ccloquet <c.cloquet@gmail.com>
2018-12-13 16:12:46 +0100

    choice between Twilio and Clickatell

--- config/params.dev.php
+++ config/params.dev.php
@@ -1,17 +1,21 @@
 <?php
 
-	define('TWILIO_SID', '');
-	define('TWILIO_APIKEY', '');
+	define('TWILIO_SID', 		'');						// Twilio : for TURN & STUN servers (always) & for SMS API (if you choose to use TWILIO below)
+	define('TWILIO_APIKEY', 	'');
+	define('TWILIO_FROM', 		'');						// might be on a customer basis, in the table below. Begins with a "+" and the country code
+
+	define('SMS_API', 		'TWILIO'); 					// TWILIO, CLICKATELL
 
 	define('BASE_URL', 		'https://****/html/showme.html');		// URL OF THE CLIENT PAGE
 
 	define('BASE_FOLDER', 		'../received/');				// FOLDER WHERE THE IMAGES ARE STORED
 	define('USAGE_FOLDER', 		'../db/');					// FOLDER WHERE THE USAGE DATA ARE STORED
 
-	define ("INITIAL_TOPUP", 10);							// by default, gives 5 credits the first time
+	define ("INITIAL_TOPUP", 	10);						// by default, gives 10 credits the first time
 
 
     $params = array_merge($params, [
+	// userids (confidential)							// secret -- allow to authentify that the link sent by SMS comes from this server
         1 =>   ['secret'=> 'azerty', 'name'=> 'test user', 'sms_apikey' => '']
     ]);
 
--- php/send_sms.php
+++ php/send_sms.php
@@ -36,8 +36,15 @@
 	// no other choice for the body => limits abuse
 	$body 		= 'Open the link to take a picture. Ouvrez le lien pour prendre une photo. Open de link om een foto te nemen. ' . BASE_URL . '?key=' . $key;
 
-	$ret 		= send_clickatell($sms_apikey, $body, $to);
-	$ret=true;
+	switch(SMS_API)
+	{
+		case 'TWILIO':
+			$ret 		= send_twilio(TWILIO_SID, TWILIO_APIKEY, TWILIO_FROM, 	$body, $to);			
+			break;
+		default:
+			$ret 		= send_clickatell($sms_apikey, 				$body, $to);
+	}
+			
 
 	if ($ret !== false)
 	{
@@ -81,6 +88,32 @@
 		{
 			return false;
 		}
+	}
+
+	function send_twilio($sid, $token, $from, $body, $to)
+	{
+		$fields 	=  ["Body"=> $body, "From"=>$from, "To"=>$to] ;
+		$ch    	 	= curl_init();
+		curl_setopt($ch, CURLOPT_URL, 			'https://api.twilio.com/2010-04-01/Accounts/'.$sid.'/Messages.json');
+		curl_setopt($ch, CURLOPT_POST, 			TRUE);
+		curl_setopt($ch, CURLOPT_USERPWD, 		$sid . ':' . $token);
+		curl_setopt($ch, CURLOPT_HTTPAUTH, 		CURLAUTH_BASIC);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,  	1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,  	false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,  	0);
+		curl_setopt($ch, CURLOPT_POSTFIELDS,     	http_build_query($fields));
+		$ret 		= curl_exec($ch);
+		curl_close($ch);
+
+		$ret 		= json_decode($ret, true);
+
+		if ( isset($ret['sid']) )
+		{
+			return true;
+		}
+
+		return false;
+
 	}
 
 
